ddbb7afb3d74883118ffebb31a7acc75242cfccf
nick <nickfarrell@gmail.com>
2019-04-15 20:07:53 -0700

    ingredient model

---
+++ app/src/main/java/com/example/bakingapp/model/Ingredient.java
@@ -0,0 +1,81 @@
+package com.example.bakingapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient implements Parcelable {
+
+//    {
+//        "quantity": 2,
+//            "measure": "CUP",
+//            "ingredient": "Graham Cracker crumbs"
+//    }
+
+    @SerializedName("quantity")
+    private Integer quantity;
+    @SerializedName("measure")
+    private String measure;
+    @SerializedName("ingredient")
+    private String ingredient;
+
+    public Ingredient(Integer quantity, String measure, String ingredient) {
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    // Parcelable implementation
+    protected Ingredient(Parcel in) {
+        quantity = in.readInt();
+        measure = in.readString();
+        ingredient = in.readString();
+    }
+
+    public static final Creator<Ingredient> CREATOR = new Creator<Ingredient>() {
+        @Override
+        public Ingredient createFromParcel(Parcel in) {
+            return new Ingredient(in);
+        }
+
+        @Override
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+}
---
+++ app/src/main/java/com/example/bakingapp/model/Recipe.java
@@ -0,0 +1,4 @@
+package com.example.bakingapp.model;
+
+public class Recipe {
+}
