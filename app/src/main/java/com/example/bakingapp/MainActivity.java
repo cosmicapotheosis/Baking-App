d2abeb60d2c83ebbcb985345e7d4f520b18bf4ee
nick <nickfarrell@gmail.com>
2019-04-20 13:19:23 -0700

    getRecyclerViewThumbnailUrls gets thumbnails for recyclerview

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -6,9 +6,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.bakingapp.model.Recipe;
+import com.example.bakingapp.model.Step;
 import com.example.bakingapp.network.RecipeService;
 import com.example.bakingapp.network.RetrofitClientInstance;
+import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
 
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -20,6 +25,9 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private RecipeService service;
+
+    private ArrayList<Recipe> mRecipes;
+    private ArrayList<String> mThumbnailUrls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +44,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<ArrayList<Recipe>> call, Response<ArrayList<Recipe>> response) {
                 if (response.isSuccessful()) {
-                    for (Recipe r : response.body()) {
-                        Log.d("MainActivity", r.getName());
-                    }
+                    mRecipes = response.body();
+                    mThumbnailUrls = getRecyclerViewThumbnailUrls(mRecipes);
                 } else {
                     try {
                         JSONObject jObjError = new JSONObject(response.errorBody().string());
@@ -54,5 +61,51 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Something went wrong... Please try later!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    /**
+     * Iterates through list of Recipes and returns a list of urls for thumbnails corresponding to each recipe
+     *
+     * @param recipes
+     * @return ArrayList of urls to use for RecyclerView thumbnails
+     */
+    private ArrayList<String> getRecyclerViewThumbnailUrls(ArrayList<Recipe> recipes) {
+        ArrayList<String> recyclerViewThumbnailUrls = new ArrayList<String>();
+
+        for (Recipe r : recipes) {
+            // If an image is not included for the recipe, get the first images from the recipe's steps
+            if (Strings.isNullOrEmpty(r.getImage())) {
+                // Use Google Guava to find first Step with a thumbnail
+                // If thumbnailURL is empty, use videoURL
+                Step stepWithVideoThumbnail = Iterables.tryFind(r.getSteps(),
+                        new Predicate<Step>() {
+                            @Override
+                            public boolean apply(Step input) {
+                                return !input.getVideoUrl().isEmpty() && input.getVideoUrl() != null;
+                            }
+                        }).orNull();
+
+                Step stepWithThumbnail = Iterables.tryFind(r.getSteps(),
+                        new Predicate<Step>() {
+                            @Override
+                            public boolean apply(Step input) {
+                                return !input.getThumbnailUrl().isEmpty() && input.getThumbnailUrl() != null;
+                            }
+                        }).orNull();
+
+                if (stepWithThumbnail != null)
+                    recyclerViewThumbnailUrls.add(stepWithThumbnail.getThumbnailUrl());
+                else
+                    recyclerViewThumbnailUrls.add(stepWithVideoThumbnail.getVideoUrl());
+            } else {
+                recyclerViewThumbnailUrls.add(r.getImage());
+            }
+        }
+        // Make sure all recipes have a thumbnail
+        if (recyclerViewThumbnailUrls.size() == recipes.size()) {
+            return recyclerViewThumbnailUrls;
+        } else {
+            return null;
+        }
     }
 }
