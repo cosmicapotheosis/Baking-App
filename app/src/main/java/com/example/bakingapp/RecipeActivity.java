f24018ac9f36e432b695c61b4993eb75e6a30295
nick <nickfarrell@gmail.com>
2019-04-25 10:38:50 -0700

    Display steps in recipeview

--- app/src/main/java/com/example/bakingapp/RecipeActivity.java
+++ app/src/main/java/com/example/bakingapp/RecipeActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 import com.example.bakingapp.model.Ingredient;
 import com.example.bakingapp.model.Recipe;
+import com.example.bakingapp.model.Step;
 import com.google.common.base.Strings;
 import com.squareup.picasso.Picasso;
 
@@ -18,7 +19,8 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity
+        implements StepAdapter.ListItemClickListener{
 
     // Constant for logging
     private static final String TAG = RecipeActivity.class.getSimpleName();
@@ -30,10 +32,14 @@ public class RecipeActivity extends AppCompatActivity {
 
     @BindView(R.id.recyclerview_ingredients)
     RecyclerView mIngredientRecyclerView;
+    @BindView(R.id.recyclerview_steps)
+    RecyclerView mStepRecyclerView;
 
     private IngredientAdapter mIngredientAdapter;
+    private StepAdapter mStepAdapter;
 
     private ArrayList<Ingredient> mIngredients;
+    private ArrayList<Step> mSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,15 +70,27 @@ public class RecipeActivity extends AppCompatActivity {
                         .into(mImageView);
             }
 
-            // Lay out recipes in a linear layout
-            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-            mIngredientRecyclerView.setLayoutManager(layoutManager);
+            // Lay out ingredients in a linear layout
+            LinearLayoutManager layoutManager1 = new LinearLayoutManager(this);
+            mIngredientRecyclerView.setLayoutManager(layoutManager1);
             mIngredientRecyclerView.setHasFixedSize(true);
             mIngredientAdapter = new IngredientAdapter();
             mIngredientRecyclerView.setAdapter(mIngredientAdapter);
+            mIngredientRecyclerView.setNestedScrollingEnabled(false);
             mIngredientAdapter.setmIngredients(mRecipe.getIngredients());
 
-            Log.d(TAG, "First ingredient" + mRecipe.getIngredients().get(0));
+            LinearLayoutManager layoutManager2 = new LinearLayoutManager(this);
+            mStepRecyclerView.setLayoutManager(layoutManager2);
+            mStepRecyclerView.setHasFixedSize(true);
+            mStepAdapter = new StepAdapter(this);
+            mStepRecyclerView.setAdapter(mStepAdapter);
+            mStepRecyclerView.setNestedScrollingEnabled(false);
+            mStepAdapter.setmSteps(mRecipe.getSteps());
         }
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
     }
 }
---
+++ app/src/main/java/com/example/bakingapp/StepAdapter.java
@@ -0,0 +1,120 @@
+package com.example.bakingapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bakingapp.model.Step;
+
+import java.util.ArrayList;
+
+public class StepAdapter extends
+        RecyclerView.Adapter<StepAdapter.StepAdapterViewHolder> {
+
+    private ArrayList<Step> mSteps;
+
+    /*
+     * An on-click handler that we've defined to make it easy for an Activity to interface with
+     * our RecyclerView
+     */
+    final private ListItemClickListener mOnClickListener;
+
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    public StepAdapter(ListItemClickListener listener) {
+        mOnClickListener = listener;
+    }
+
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param viewGroup The ViewGroup that these ViewHolders are contained within.
+     * @param i  If your RecyclerView has more than one type of item (which ours doesn't) you
+     *                  can use this viewType integer to provide a different layout. See
+     *                  {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                  for more details.
+     * @return A new StepAdapterViewHolder that holds the View for each list item
+     */
+    @NonNull
+    @Override
+    public StepAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.step_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new StepAdapterViewHolder(view);
+    }
+
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the step
+     * details for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param stepAdapterViewHolder The ViewHolder which should be updated to represent the
+     *                                  contents of the item at the given position in the data set.
+     * @param i                  The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull StepAdapterViewHolder stepAdapterViewHolder, int i) {
+        Step step = mSteps.get(i);
+        // Set TextViews..
+        stepAdapterViewHolder.mStepShortDescTextView.setText(step.getShortDescription());
+    }
+
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our steps list
+     */
+    @Override
+    public int getItemCount() {
+        if (null == mSteps) return 0;
+        return mSteps.size();
+    }
+
+    /**
+     * Sets steps
+     * @param steps
+     */
+    public void setmSteps(ArrayList<Step> steps) {
+        mSteps = steps;
+        notifyDataSetChanged();
+    }
+
+    public class StepAdapterViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        public final TextView mStepShortDescTextView;
+
+        public StepAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mStepShortDescTextView = (TextView) itemView.findViewById(R.id.tv_step_short_desc);
+            itemView.setOnClickListener(this);
+        }
+
+        /**
+         * Called whenever a user clicks on an item in the list.
+         * @param v The View that was clicked
+         */
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+    }
+}
